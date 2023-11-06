6fb9aa62172df33127dc32534e4e608d3caabd0f
terrencebudnik <terrence.budnik@gmail.com>
2023-11-06 16:51:30 -0600

    css for app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
-import Banner from "../components/Banner";
 import Instructions from "../components/Instructions";
 import Button from "react-bootstrap/Button";
 import "./Home.css";
@@ -11,17 +10,11 @@ function Home() {
 
   return (
     <Container fluid className="home-page">
-      <Row className="home-page-banner-row">
-        <Col>
-      
-        </Col>
-      </Row>
       <Row className="home-page-header-row">
         <Col>
           <h1 className="home-page-header">What's the Scuttlebutt?</h1>
         </Col>
       </Row>
-
       <Row className="how-to-play-row">
         <Col>
           <Button
